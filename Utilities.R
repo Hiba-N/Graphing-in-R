af31eefa1e12897b95e6a740f3d0eac25e13af6a
Hiba-N <hibanasir123@hotmail.com>
2026-03-04 22:56:23 -0500

    completed bacon df creating function

--- Utilities.R
+++ Utilities.R
@@ -1,23 +1,23 @@
 source("constants.R")
+library("data.table") #for %like% operator
+library(tidyr)
 
-#  Create a function which loads the data above and extracts the "bacon" row and puts it into a data frame.
-# The function should also create a column which contains the months. 
-# The months should be indicated by month number, starting at 1, 
-# but not repeating (1 through the maximum number of months). 
-# You may hard-code the name of the file. Note that this file is a bit of a mess, 
-# you will need to add additional flags to your read.csv call to get the data into memory. 
-# Also note that the as.numeric function can be used to convert a data frame row into a vector.
+
 
 read_in.file <- function(){
     all.data <- read.csv(DATA, sep = ",", skip=HEADER_LENGTH) #skipping header rows
-    #View(all.data) #sanity check
     all.data <- drop_na(all.data) #dropping invalid data, (essentially to drop footer)
-    #View(all.data) # sanity check
     return(all.data)
 }
 
-read_in.bacon <- function(){
-    
+read_in.bacon <- function(all.data){
+    bacon.data <- all.data[all.data[[1]] %like% "Bacon",]
+    bacon.price <- as.numeric(bacon.data[-1]) #getting bacon prices as a list, removing bacon name in first cell
+    print(bacon.price)
+    months <- c(1:length(bacon.price)) #creating months list
+    bacon.data <- cbind(months, bacon.price) #creating a bacon df of its own, consisting of months and bacon prices
+    View(bacon.data)
 }
 
-read_in.file()
+data <- read_in.file()
+read_in.bacon(data)
--- generateModels.R
+++ generateModels.R
@@ -1,21 +1,30 @@
 source("constants.R")
+source("utilities.R")
 
 args <- commandArgs(trailingOnly= TRUE)
 
-if(length(args) != 1 || args not %in% ALLOWED_ARGS){
+
+
+generate.bacon.models <- function(args){
+
+    all.data <- read_in.file()
+
+    if(length(args) != 1 || args not %in% ALLOWED_ARGS){
     cat("usage example: Rscript generateModels.R 1/2/3\n")
     quit()
 }
 
+    if (args[1] == 1){
+        cat("Command found: ", args[1]), "\ninitiating (1)"
+    }
 
-if (args[1] == 1){
-    cat("Command found: ", args[1]), "\ninitiating (1)"
+    if (args[1] == 2){
+        cat("Command found: ", args[1]), "\ninitiating (2)"
+    }
+
+    if (args[1] == 3){
+        cat("Command found: ", args[1]), "\ninitiating (3)"
+    }
 }
 
-if (args[1] == 2){
-    cat("Command found: ", args[1]), "\ninitiating (2)"
-}
-
-if (args[1] == 3){
-    cat("Command found: ", args[1]), "\ninitiating (3)"
-}
+generate.bacon.models(args)
